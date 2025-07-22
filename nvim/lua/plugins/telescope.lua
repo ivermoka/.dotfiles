569f7e74472e24947ed4d7977e3260b8f2a82398
Iver Morland Karlsvik <iver.karlsvik@statnett.no>
2025-07-22 16:23:17 +0200

    feat: working version

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,45 +1,10 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.5",
-  dependencies = { "nvim-lua/plenary.nvim" },
-
-  keys = {
-    {
-      "<C-p>",
-      function()
-        require("telescope.builtin").find_files()
-      end,
-      desc = "Find Files",
-    },
-    {
-      "<leader>fg",
-      function()
-        require("telescope.builtin").live_grep()
-      end,
-      desc = "Live Grep",
-    },
-    {
-      "<leader>fb",
-      function()
-        require("telescope.builtin").buffers()
-      end,
-      desc = "Find Buffers",
-    },
-    {
-      "<leader>fh",
-      function()
-        require("telescope.builtin").help_tags()
-      end,
-      desc = "Help Tags",
-    },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    config = function()
+      require("telescope").setup()
+    end,
   },
-
-  opts = {
-    defaults = {
-      layout_strategy = "vertical",
-      layout_config = {
-        vertical = { width = 0.5 },
-      },
-    },
-  },
-}
+  }
