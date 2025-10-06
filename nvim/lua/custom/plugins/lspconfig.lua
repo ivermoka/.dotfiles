9f4eb9a7f97701cbcc6c1f5d450bf95ce6493f93
Iver Morland Karlsvik <iver.karlsvik@statnett.no>
2025-10-06 11:24:19 +0200

    fix: think i fixed kotlin now?

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -209,10 +209,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         gopls = {},
-        kotlin_language_server = {
-          cmd = { 'kotlin-language-server' },
-          filetypes = { 'kotlin' },
-        },
+        kotlin_language_server = {},
         jdtls = {},
 
         ts_ls = {},
