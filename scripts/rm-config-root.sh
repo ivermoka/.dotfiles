0ae5b1f5ced9392b7eae565f8626ee7b2e778660
Iver Morland Karlsvik <iver.karlsvik@statnett.no>
2026-04-22 12:07:01 +0200

    feat(scripts): updated

---
+++ scripts/rm-config-root.sh
@@ -0,0 +1,6 @@
+rm ~/.bashrc
+rm -rf ~/.bashrc.d
+rm ~/.zshrc
+rm ~/.zsh_plugins.txt
+rm ~/.config/starship.toml
+rm ~/.tmux.conf
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
-ln -s ~/projects/.dotfiles/config/.bashrc ~/.bashrc
-ln -s ~/projects/.dotfiles/config/.bashrc.d ~/.bashrc.d
-ln -s ~/projects/.dotfiles/config/.zshrc ~/.zshrc
-ln -s ~/projects/.dotfiles/config/.tmux.conf ~/.tmux.conf
+# Zsh
+ln -sf ~/projects/.dotfiles/config/shell/.zshrc ~/.zshrc
+ln -sf ~/projects/.dotfiles/config/shell/.zsh_plugins.txt ~/.zsh_plugins.txt
+ln -sfn ~/projects/.dotfiles/config/shell/zsh ~/.config/zsh
+
+# Starship <Zsh>
+ln -sf ~/projects/.dotfiles/config/shell/starship.toml ~/.config/starship.toml
+
+# Tmux
+ln -sf ~/projects/.dotfiles/config/tmux/.tmux.conf ~/.tmux.conf
