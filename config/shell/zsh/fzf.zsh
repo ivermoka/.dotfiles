a476fca152cf0c1f2513cc88cd86d919fab42f17
ivermoka <ivermoka@icloud.com>
2026-08-06 15:20:16 +0200

    fix(fzf): avoid ongoing issue with fzf-tab

--- config/shell/zsh/fzf.zsh
+++ config/shell/zsh/fzf.zsh
@@ -1,3 +1,5 @@
+autoload -Uz compinit; compinit  # Added to avoid fzf-tab errors. Look at https://github.com/Aloxaf/fzf-tab/issues/529
+
 if [[ ! "$PATH" == */home/iverk/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/home/iverk/.fzf/bin"
 fi
