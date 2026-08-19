fd1f7be9a8670645ebbae263d40219bcf58fa1fd
ivermoka <iver.morland.karlsvik@tieto.com>
2026-08-19 15:15:49 +0200

    feat(setup): refine

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
+# Clone repos
+git clone git@github.com:ivermoka/.dotfiles.git ~/projects/.dotfiles
+git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
+
 # Zsh
 ln -sf ~/projects/.dotfiles/config/shell/.zshrc ~/.zshrc
 ln -sf ~/projects/.dotfiles/config/shell/.zsh_plugins.txt ~/.zsh_plugins.txt
@@ -8,3 +12,6 @@ ln -sf ~/projects/.dotfiles/config/shell/starship.toml ~/.config/starship.toml
 
 # Tmux
 ln -sf ~/projects/.dotfiles/config/tmux/.tmux.conf ~/.tmux.conf
+
+# Git
+ln -sf ~/projects/.dotfiles/config/git/.gitconfig ~/.gitconfig
