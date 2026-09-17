ba28c16ad22293f41ac181be884954e7da398617
ivermoka <iver.morland.karlsvik@tieto.com>
2026-09-17 10:07:12 +0200

    feat(aliases): mvn helpers

--- config/shell/zsh/aliases.zsh
+++ config/shell/zsh/aliases.zsh
@@ -8,3 +8,11 @@ alias v.="nvim ."
 alias la="ls -a"
 alias javav="sudo update-alternatives --config java"
 
+# Helpers for Spring Boot / Flyway / Maven
+alias clean='mvn flyway:clean "-Dflyway.url=jdbc:oracle:thin:@//localhost:1521/orcl" "-Dflyway.user=appdata" "-Dflyway.password=app" "-Dflyway.cleanDisabled=false"'
+alias migrate='mvn flyway:migrate "-Dflyway.url=jdbc:oracle:thin:@//localhost:1521/orcl" "-Dflyway.user=appdata" "-Dflyway.password=app"'
+alias repair='mvn flyway:repair "-Dflyway.url=jdbc:oracle:thin:@//localhost:1521/orcl" "-Dflyway.user=appdata" "-Dflyway.password=app"'
+alias run='mvn spring-boot:run'
+alias ccompile='mvn clean compile'
+alias cinstall='mvn clean install'
+
